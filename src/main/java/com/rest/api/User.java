44fbc580f8996d46fba98726c9e8729ceb99b44d
Эдуард Гарайс <garais.eduard@gmail.com>
2023-03-26 18:51:39 +0300

    Added UserController, User Entity, UserRepository.

---
+++ src/main/java/com/rest/api/User.java
@@ -0,0 +1,61 @@
+package com.rest.api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+import java.net.URI;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    URI image;
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    UUID id;
+    String username, password, email;
+
+    public URI getImage() {
+        return image;
+    }
+
+    public void setImage(URI image) {
+        this.image = image;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ src/main/java/com/rest/api/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.rest.api.controller;
+
+import com.rest.api.User;
+import com.rest.api.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public @ResponseBody UserRepository findAllUsers() {
+        return userRepository;
+    }
+
+    @GetMapping("{userID}")
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody Optional<User> findUserByID(@PathVariable UUID userID) {
+        return userRepository.findById(userID);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody Map<String, String> addNewUser (@RequestBody User user) {
+        userRepository.save(user);
+        return Map.of("message", "success");
+    }
+}
